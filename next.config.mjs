e54da052641a135eaa9e0c0e3db3b91f56badeab
Liam <71823298+liamjameswatson@users.noreply.github.com>
2025-07-19 15:43:12 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "helmgrpgdzvuklgljzpy.supabase.co",
+        hostname: "iyqnxorztpaltmyobseu.supabase.co",
         port: "",
         pathname: "/storage/v1/object/public/cabin-images/**",
       },
