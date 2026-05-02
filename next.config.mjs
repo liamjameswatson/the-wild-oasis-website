75517615d247fa14944592255af3bb243e309064
Liam <71823298+liamjameswatson@users.noreply.github.com>
2026-05-02 18:23:35 +0100

    Update Supabase hostname in next.config.mjs
    a new supabase project added due to expiry time

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "iyqnxorztpaltmyobseu.supabase.co",
+        hostname: "pkxlpxkbjzxxaqlsslrn.supabase.co",
         port: "",
         pathname: "/storage/v1/object/public/cabin-images/**",
       },
